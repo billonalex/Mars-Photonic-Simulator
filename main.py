902ae5897dbde81c417c906fc7b9e83c353ba303
Alexandre Billon <abi@atolcd.com>
2021-05-19 13:13:57 +0200

    [Update] Documentation

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from model import MarsModel
+from .model import MarsModel
 
 if(__name__ == "__main__"):
+
+    """
+    Here is the first option to declare and make a simulation !
+    """
+
     my_model = MarsModel(baseline=10, diameter=1)
 
     my_model.calculate()
--- model.py
+++ model.py
@@ -8,12 +8,29 @@ class MarsModel:
 
     #Constructor
     def __init__(self, baseline, diameter):
+        #We initiate our parameters here
         self.baseline = baseline
         self.diameter = diameter
+
+        #We can declare and calculate different paramaters that we may need in the future
+
 
     def calculate(self):
         print('Function to make the simulation')
 
+        """
+        The idea is to put the biggest calculations here, and to add optional parameters to personalize our model
+        Maybe we will want to make calculation many times
+        2 possibilities in the main script :
+
+            - Declare only one model and personalize it with parameters required
+            - Declare one model per personalization
+        """
+
     #Show result
     def show(self):
         print('Function to plot the result')
+
+        """
+        When the calculs are done, this function allows us to show the results.
+        """
